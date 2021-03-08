372b8cdc6d2f0e89bdc453551ff88d245020af4f
Yash Pratap <yashp241195@gmail.com>
2021-03-08 19:57:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(
+    'mongodb://localhost/traveldb',
+    { useNewUrlParser: true,  useUnifiedTopology: true }
+)
+
+const app = express()
+
+app.use(morgan('dev'));
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.1', 
+      info: {
+        version: "1.0.0",
+        title: "API",
+        description: "API Information for tourist app",
+        contact: {
+          name: "Yash Pratap"
+        },
+        servers: ["http://localhost:3000"]
+      },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'jwt',
+          }
+        }
+      },
+      security: [{
+        bearerAuth: []
+      }]
+    },
+    apis: [
+      "./docs/**/*.yaml",      
+    ]
+};
+  
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use('/place', require('./api/route/place'))
+
+app.post('/test',(req, res)=>{
+
+  console.log('testing ...')
+  const {test, name} = req.body
+  console.log(`value of test = ${JSON.stringify(test, null, 2)}`)
+  console.log(`value of name = ${JSON.stringify(name, null, 2)}`)
+
+  res.json({
+    r:{name:'qweqwe'}
+  })
+
+})
+
+app.listen(3005,()=>{
+  console.log('Welcome to tourist app')
+})
+
